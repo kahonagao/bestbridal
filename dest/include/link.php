cec4eca2a5203e12dabafbc1c29d89490f7b519d
“kahonagao” <kahonagao@gmail.com>
2020-05-06 10:52:54 +0900

    t commit# 以前製作していたファイルをコミット

---
+++ dest/include/link.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="/common/css/ress.css">
+<link rel="stylesheet" href="/common/css/print.css">
+<link rel="stylesheet" href="/common/css/base.css">
+<link rel="stylesheet" href="/common/css/component.min.css">
+<link rel="stylesheet" href="/common/css/fontawesome-all.min.css">
+<link rel="stylesheet" href="/common/js/drawer/drawer.min.css">
+<!--<link rel="icon" href="/lib/cmn_img/favicon/favicon.ico" type="image/vnd.microsoft.icon">-->
+<script src="/common/js/jquery.min.js"></script>
